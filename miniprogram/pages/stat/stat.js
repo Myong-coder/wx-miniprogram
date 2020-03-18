ddd343b44d1e8898a1e5d56141f5219ebfa47815
unknown <unknown@example.com>
2020-03-18 18:21:49 +0800

    统计图样式设计完成

--- miniprogram/pages/stat/stat.js
+++ miniprogram/pages/stat/stat.js
@@ -2,6 +2,66 @@ import * as echarts from '../../ec-canvas/echarts';
 
 let chart = null;
 
+const db = wx.cloud.database()
+const tasks = db.collection('tasks')
+var dataList1 = []
+var dataList2 = []
+var dataList3 = []
+Page({
+  data: {
+    todos: [],
+    ec: {
+      onInit: initChart,
+      //lazyLoad: true
+    }
+  },
+  onLoad: function (options) {
+    this.echartsComponent = this.selectComponent('#mychart')
+    this.getData();
+  },
+
+  getData: function () {
+    tasks.get().then(res => {
+      //console.log("全部数据:",res.data)
+      this.setData({
+        todos: res.data
+      })
+      console.log(this.data.todos[0].tomatoTime)
+    })
+    let sum = 0
+    let sum1 = 0
+    let sum2 = 0
+    var arr = this.data.todos;
+    console.log(arr)
+    console.log(arr.length)
+    for (let i = 0; i < arr.length; i++) {
+      console.log(arr[i].tomatoTime)
+      sum = sum + arr[i].tomatoTime
+      console.log(sum)
+      console.log(arr.isFinish)
+      if (arr.isFinish === true) {
+        sum1 = sum1 + 1
+        console.log(sum1)
+      }
+      else {
+        sum2 = sum2 + 1
+        console.log(sum2)
+      }
+    }
+    dataList1 = [10]
+    dataList2 = [20]
+    dataList3 = [30]
+    //console.log(dataList1,dataList2,dataList3)
+  },
+
+  onReady() {
+    setTimeout(function () {
+      // 获取 chart 实例的方式
+      //console.log(chart)
+    }, 2000);
+  }
+});
+
 function initChart(canvas, width, height, dpr) {
   chart = echarts.init(canvas, null, {
     width: width,
@@ -46,7 +106,7 @@ function initChart(canvas, width, height, dpr) {
       {
         type: 'category',
         axisTick: { show: false },
-        data: ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期天'],
+        data: ['任务记录'],
         axisLine: {
           lineStyle: {
             color: '#999'
@@ -67,7 +127,7 @@ function initChart(canvas, width, height, dpr) {
             position: 'inside'
           }
         },
-        data: [300, 270, 340, 344, 300, 320, 310],
+        data: dataList1,
         itemStyle: {
           // emphasis: {
           //   color: '#37a2da'
@@ -83,7 +143,7 @@ function initChart(canvas, width, height, dpr) {
             show: true
           }
         },
-        data: [120, 102, 141, 174, 190, 250, 220],
+        data: dataList2,
         itemStyle: {
           // emphasis: {
           //   color: '#32c5e9'
@@ -100,7 +160,7 @@ function initChart(canvas, width, height, dpr) {
             position: 'left'
           }
         },
-        data: [-10, -32, -21, -34, -90, -130, -110],
+        data: dataList3,
         itemStyle: {
           // emphasis: {
           //   color: '#67e0e3'
@@ -114,18 +174,5 @@ function initChart(canvas, width, height, dpr) {
   return chart;
 }
 
-Page({
-  data: {
-    monday: 10,
-    ec: {
-      onInit: initChart
-    }
-  },
 
-  onReady() {
-    setTimeout(function () {
-      // 获取 chart 实例的方式
-      // console.log(chart)
-    }, 2000);
-  }
-});
+
