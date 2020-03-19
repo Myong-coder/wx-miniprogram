8d5ca1bd94ef5742f625d8ba49858f53f3522d7b
unknown <unknown@example.com>
2020-03-19 13:52:40 +0800

    边边角角

--- miniprogram/pages/stat/stat.js
+++ miniprogram/pages/stat/stat.js
@@ -5,20 +5,22 @@ const db = wx.cloud.database();
 const tasks = db.collection('tasks');
 var chartData = {
   main: {
-    title: '总成交量',
-    data: [15, 20, 45, 37],
+    title: '任务记录',
+    data: [15, 20, 45],
     categories: ['已完成', '未完成', '番茄记录']
   }
 };
 var dataList =[]
+var sum = 0
+var sum1 = 0
+var sum2 = 0
 Page({
   data: {
     todos:[],
     chartTitle: '任务完成记录',
     isMainChartDisplay: true
   },
-
-  async onReady(e) {
+  async getData(){
     await tasks.get().then(res => {
       //console.log("全部数据:",res.data)
       this.setData({
@@ -26,9 +28,7 @@ Page({
       })
       //console.log(this.data.todos[0].tomatoTime)
     })
-    let sum = 0
-    let sum1 = 0
-    let sum2 = 0
+
     var arr = this.data.todos;
     //console.log(arr)
     console.log(arr.length)
@@ -50,8 +50,45 @@ Page({
       }
     }
     console.log(sum, sum2, sum1)
-    dataList = [sum1,sum2,sum]
-    chartData.main.data=dataList
+    dataList = [sum1, sum2, sum]
+    chartData.main.data = dataList
+  },
+
+  async onReady(e) {
+    // await tasks.get().then(res => {
+    //   //console.log("全部数据:",res.data)
+    //   this.setData({
+    //     todos: res.data
+    //   })
+    //   //console.log(this.data.todos[0].tomatoTime)
+    // })
+    // let sum = 0
+    // let sum1 = 0
+    // let sum2 = 0
+    // var arr = this.data.todos;
+    // //console.log(arr)
+    // console.log(arr.length)
+    // console.log(arr[1].isFinish)
+    // for (let i = 0; i < arr.length; i++) {
+    //   //console.log(arr[i].tomatoTime)
+    //   if (arr[i].tomatoTime != undefined) {
+    //     sum = sum + arr[i].tomatoTime
+    //     //console.log(sum)
+    //   }
+    //   //console.log(arr[i].isFinish)
+    //   if (arr[i].isFinish) {
+    //     sum1 = sum1 + 1
+    //     //console.log(sum1)
+    //   }
+    //   else {
+    //     sum2 = sum2 + 1
+    //     //console.log(sum2)
+    //   }
+    // }
+    // console.log(sum, sum2, sum1)
+    // dataList = [sum1,sum2,sum]
+    // chartData.main.data=dataList
+    await this.getData();
     var windowWidth = 400;
     columnChart = new wxCharts({
       canvasId: 'columnCanvas',
@@ -84,5 +121,5 @@ Page({
       width: windowWidth,
       height: 500,
     });
-  }
+  },
 });
