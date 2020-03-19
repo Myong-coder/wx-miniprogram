44571aece930876cb0a8659c7704128d68d0f1c7
unknown <unknown@example.com>
2020-03-19 12:50:26 +0800

    完成统计图

--- miniprogram/pages/stat/stat.js
+++ miniprogram/pages/stat/stat.js
@@ -1,178 +1,88 @@
-import * as echarts from '../../ec-canvas/echarts';
-
-let chart = null;
-
-const db = wx.cloud.database()
-const tasks = db.collection('tasks')
-var dataList1 = []
-var dataList2 = []
-var dataList3 = []
+var wxCharts = require('../../utils/wxcharts.js');
+var app = getApp();
+var columnChart = null;
+const db = wx.cloud.database();
+const tasks = db.collection('tasks');
+var chartData = {
+  main: {
+    title: '总成交量',
+    data: [15, 20, 45, 37],
+    categories: ['已完成', '未完成', '番茄记录']
+  }
+};
+var dataList =[]
 Page({
   data: {
-    todos: [],
-    ec: {
-      onInit: initChart,
-      //lazyLoad: true
-    }
-  },
-  onLoad: function (options) {
-    this.echartsComponent = this.selectComponent('#mychart')
-    this.getData();
+    todos:[],
+    chartTitle: '任务完成记录',
+    isMainChartDisplay: true
   },
 
-  getData: function () {
-    tasks.get().then(res => {
+  async onReady(e) {
+    await tasks.get().then(res => {
       //console.log("全部数据:",res.data)
       this.setData({
         todos: res.data
       })
-      console.log(this.data.todos[0].tomatoTime)
+      //console.log(this.data.todos[0].tomatoTime)
     })
     let sum = 0
     let sum1 = 0
     let sum2 = 0
     var arr = this.data.todos;
-    console.log(arr)
+    //console.log(arr)
     console.log(arr.length)
+    console.log(arr[1].isFinish)
     for (let i = 0; i < arr.length; i++) {
-      console.log(arr[i].tomatoTime)
-      sum = sum + arr[i].tomatoTime
-      console.log(sum)
-      console.log(arr.isFinish)
-      if (arr.isFinish === true) {
+      //console.log(arr[i].tomatoTime)
+      if (arr[i].tomatoTime != undefined) {
+        sum = sum + arr[i].tomatoTime
+        //console.log(sum)
+      }
+      //console.log(arr[i].isFinish)
+      if (arr[i].isFinish) {
         sum1 = sum1 + 1
-        console.log(sum1)
+        //console.log(sum1)
       }
       else {
         sum2 = sum2 + 1
-        console.log(sum2)
+        //console.log(sum2)
       }
     }
-    dataList1 = [10]
-    dataList2 = [20]
-    dataList3 = [30]
-    //console.log(dataList1,dataList2,dataList3)
-  },
-
-  onReady() {
-    setTimeout(function () {
-      // 获取 chart 实例的方式
-      //console.log(chart)
-    }, 2000);
+    console.log(sum, sum2, sum1)
+    dataList = [sum1,sum2,sum]
+    chartData.main.data=dataList
+    var windowWidth = 400;
+    columnChart = new wxCharts({
+      canvasId: 'columnCanvas',
+      type: 'column',
+      animation: true,
+      categories: chartData.main.categories,
+      series: [{
+        name: '完成量',
+        data: chartData.main.data,
+        format: function (val, name) {
+          return val.toFixed(2) ;
+        }
+      }],
+      yAxis: {
+        format: function (val) {
+          return val;
+        },
+        title: '',
+        min: 0
+      },
+      xAxis: {
+        disableGrid: false,
+        type: 'calibration'
+      },
+      extra: {
+        column: {
+          width: 40
+        }
+      },
+      width: windowWidth,
+      height: 500,
+    });
   }
 });
-
-function initChart(canvas, width, height, dpr) {
-  chart = echarts.init(canvas, null, {
-    width: width,
-    height: height,
-    devicePixelRatio: dpr // new
-  });
-  canvas.setChart(chart);
-
-  var option = {
-    color: ['#37a2da', '#32c5e9', '#67e0e3'],
-    tooltip: {
-      trigger: 'axis',
-      axisPointer: {            // 坐标轴指示器，坐标轴触发有效
-        type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
-      },
-      confine: true
-    },
-    legend: {
-      data: ['番茄记录', '完成', '未完成']
-    },
-    grid: {
-      left: 20,
-      right: 20,
-      bottom: 15,
-      top: 40,
-      containLabel: true
-    },
-    xAxis: [
-      {
-        type: 'value',
-        axisLine: {
-          lineStyle: {
-            color: '#999'
-          }
-        },
-        axisLabel: {
-          color: '#666'
-        }
-      }
-    ],
-    yAxis: [
-      {
-        type: 'category',
-        axisTick: { show: false },
-        data: ['任务记录'],
-        axisLine: {
-          lineStyle: {
-            color: '#999'
-          }
-        },
-        axisLabel: {
-          color: '#666'
-        }
-      }
-    ],
-    series: [
-      {
-        name: '番茄记录',
-        type: 'bar',
-        label: {
-          normal: {
-            show: true,
-            position: 'inside'
-          }
-        },
-        data: dataList1,
-        itemStyle: {
-          // emphasis: {
-          //   color: '#37a2da'
-          // }
-        }
-      },
-      {
-        name: '完成',
-        type: 'bar',
-        stack: '总量',
-        label: {
-          normal: {
-            show: true
-          }
-        },
-        data: dataList2,
-        itemStyle: {
-          // emphasis: {
-          //   color: '#32c5e9'
-          // }
-        }
-      },
-      {
-        name: '未完成',
-        type: 'bar',
-        stack: '总量',
-        label: {
-          normal: {
-            show: true,
-            position: 'left'
-          }
-        },
-        data: dataList3,
-        itemStyle: {
-          // emphasis: {
-          //   color: '#67e0e3'
-          // }
-        }
-      }
-    ]
-  };
-
-  chart.setOption(option);
-  return chart;
-}
-
-
-
